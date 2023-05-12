ec57de87196720c22d03428317d176f902924b2a
aditya_ahirwar <adityaahirwar3@gmail.com>
2023-05-12 17:14:30 +0530

    Input form added

--- src/components/calendar/Calendar.jsx
+++ src/components/calendar/Calendar.jsx
@@ -4,10 +4,7 @@ import Fullcalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import interactionPlugin from "@fullcalendar/interaction";
-import { useState, useEffect, useRef} from "react";
-import { v4 as uuid } from "uuid";
-
-
+import { useState, useRef} from "react";
 const Wrapper = styled.div`
 width : 100%;
 padding-top : 10px;
@@ -16,24 +13,85 @@ padding-right : 10px;
 .fc-header-toolbar{
     font-size : 13px;
 }
+position : relative;
+`
+const Popup = styled.div`
+position : absolute;
+top : 0;
+left : 0;
+right : 0;
+bottom : 0;
+z-index : 10;
+// background-color : rgba(128, 128, 128, 0.62);
+display : flex;
+justify-content : center;
+align-items : center;
+`
+const Form = styled.form`
+display : flex;
+flex-direction : column;
+justify-content : center;
+align-items : center;
+gap : 20px;
+height : 300px;
+width : 300px;
+background-color : #cacacc;
+
+-webkit-box-shadow: 0px 0px 4px 1px rgba(0,0,0,1);
+-moz-box-shadow: 0px 0px 4px 1px rgba(0,0,0,1);
+box-shadow: 0px 0px 4px 1px rgba(0,0,0,1);
+
+
 `
 
+const Input = styled.input`
+height : 30px;
+border : none;
+width : 170px;
+border-radius : 4px;
+`
+const Submit = styled.div`
+display : flex;
+height : 30px;
+width : 170px;
+background-color : blue;
+font-size : 16px;
+color : white;
+align-items : center;
+justify-content : center;
+border-radius : 4px;
+
+`
 
 
 const Calendar = () => {
 const calendarRef = useRef(null);
+const [currEvent, setEvent] = useState('');
+const [open, setOpen] = useState(false);
+const [currDate, setCurrDate] = useState({})
+const [currAllDay, setCurrDay] = useState({})
+const [currTime, setCurrTime] = useState({})
+const [currDuration, setCurrDuration] = useState(0)
 
 const handleDateClick = (arg) => {
-  const title = prompt('Enter event title:');
-  if (title) {
-    const calendarApi = calendarRef.current.getApi();
-    calendarApi.addEvent({
-      title,
-      start: arg.date,
-      allDay: arg.allDay
-    });
-  }
+  setCurrDate(arg.date)
+  setCurrDay(arg.allDay)
+  setOpen(true)
 };
+
+const handleSubmit = (e) =>{
+e.preventDefault();
+const calendarApi = calendarRef.current.getApi();
+calendarApi.addEvent({
+  title : currEvent,
+  time : currTime,
+  duration : currDuration,
+  start: currDate,
+  allDay : currAllDay
+});
+setOpen(false)
+}
+
 
 
 
@@ -53,7 +111,19 @@ const handleDateClick = (arg) => {
         dateClick={handleDateClick}
         ref={calendarRef}
       />
+      {open && 
+      <Popup>
+        <Form>
+          <Input placeholder="Patient's Name" name="name" onChange={(e) => {e.preventDefault();setEvent(e.target.value)}}/>
+          <Input type="date" onChange={(e) => {setCurrDate(e.target.value)}}/>
+          <Input type="time" onChange={(e) => {setCurrTime(e.target.value)}}/>
+          <Input type="number" onChange={(e) => {setCurrDuration(e.target.value)}}/>
+          <Submit onClick={handleSubmit}>submit</Submit>
+        </Form>
+        </Popup>
+      }
     </Wrapper>
+
 
 
   );
